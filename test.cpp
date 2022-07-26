b9972924d368851b28be5821f0ad7ce1eb374b9a
zjuadurey <1721966349@qq.com>
2022-07-26 16:36:05 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector<int> a(18,22);
+    
+    cout << "size: " << a.size() << endl;
+    cout << "a[0]: " << a[0] << endl;
+    return 0;
+}
